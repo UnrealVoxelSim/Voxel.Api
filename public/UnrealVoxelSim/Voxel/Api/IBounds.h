f665f90c1f5d1e4cb3ce47718998b5e87fd55c41
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-08-29 19:33:04 +0200

    TODOs added

--- public/UnrealVoxelSim/Voxel/Api/IBounds.h
+++ public/UnrealVoxelSim/Voxel/Api/IBounds.h
@@ -4,8 +4,6 @@
 
 namespace UnrealVoxelSim::Voxel::Api
 {
-	// TODO The role of this interface is unclear. "IBounds" is too generic and does not express
-	// what object's bounds are being queried or which cross-domain use case this abstraction serves.
 	class IBounds
 	{
 	public:
